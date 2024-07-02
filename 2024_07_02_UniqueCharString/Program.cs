d7d4f85a70d0be6a70c575cb1663f5492e69a283
Steven Shao <stevenshao2016@icloud.com>
2024-07-02 19:38:55 +0800

    update

--- 2024_07_02_UniqueCharString/Program.cs
+++ 2024_07_02_UniqueCharString/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using TestNamespace;
+string testStr = "abccdefg";
+var result = UniqueCharString.IsUniqueCharString(testStr);
+Console.WriteLine(result);
+
 Console.WriteLine("Hello, World!");
---
+++ 2024_07_02_UniqueCharString/UniqueCharString.cs
@@ -0,0 +1,16 @@
+namespace TestNamespace
+{
+    public class UniqueCharString
+    {
+        public static bool IsUniqueCharString(string s)
+        {
+            Dictionary<char, int> tracker = new Dictionary<char, int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                tracker[s[i]] = tracker.GetValueOrDefault(s[i]) + 1;
+            }
+
+            return !tracker.Any(kv => kv.Value > 1);
+        }
+    }
+}
